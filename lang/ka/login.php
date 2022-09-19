c763c24d457482365b12f2d41fbf32d886b82a0e
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-09-19 22:31:07 +0400

    feat: add static text to login and register pages

---
+++ lang/ka/login.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'welcome_back'                                      => 'მოგესალმებით',
+	'enter_your_details'                                => 'მოგესალმებით! გთხოვთ შეიყვანოთ თქვენი მონაცემები',
+	'username'                                          => 'მომხმარებლის სახელი',
+	'password'                                          => 'პაროლი',
+	'remember_this_device'                              => 'მოწყობილობის დამახსოვრება',
+	'forgot_password'                                   => 'დაგავიწყდა პაროლი?',
+	'login'                                             => 'შესვლა',
+	'dont_have_an_account'                              => 'არ გაქვს ექაუნთი?',
+	'sign_up_for_free'                                  => 'დარეგისტრირდი უფასოდ',
+	'enter_unique_username_or_email'                    => 'შეიყვანე მომხმარებლის სახელი ან ელ-ფოსტა',
+	'fill_in_password'                                  => 'შეიყვანე პაროლი',
+];
