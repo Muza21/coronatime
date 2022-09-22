38119b7992da5ef90d574d2588091de9b95f1565
Lasha-Muza <lashamuzashvili@redberry.ge>
2022-09-22 13:10:42 +0400

    feat: add search icon

--- resources/views/by-country.blade.php
+++ resources/views/by-country.blade.php
@@ -12,8 +12,17 @@
         <section class="mx-16">
             <div class="mt-10">
                 <form method="GET" action="#">
-                    <input type="text" class="border-2 border-gray-100 rounded-lg pl-10 py-3" name="search"
-                        placeholder="Search by country" value="{{ request('search') }}">
+                    <div class="relative">
+                        <div class="flex absolute items-center mt-4 ml-3 pointer-events-none">
+                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
+                                stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                        </div>
+                        <input type="text" class="block border-2 border-gray-100 rounded-lg pl-10 py-3"
+                            name="search" placeholder="Search by country" value="{{ request('search') }}">
+                    </div>
                 </form>
             </div>
             <main class="rounded-md mt-10 border border-gray-200">
---
+++ resources/views/test-view.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+
+</head>
+
+<body>
+
+
+    <form>
+        <div class="relative">
+            <div class="flex absolute items-center mt-4 ml-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none"
+                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+            </div>
+            <input type="text" class="block border-2 border-gray-100 rounded-lg pl-10 py-3" name="search"
+                placeholder="Search by country" value="{{ request('search') }}">
+        </div>
+    </form>
+
+
+
+</body>
+
+</html>
